bd1977eccabd54a957879462f2fa1d44260c04c6
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-21 22:38:31 +0300

    Start using Eslint && create a function that to get weather data from weather api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+const city = prompt("Enter city");
+async function getWeather(city) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=88b16aef14f243249a9224456241905&q=${city}&days=4`,
+    { mode: "cors" }
+  );
+  const weather = await response.json();
+
+  console.log(
+    `the weather for ${weather.forecast.forecastday[1].date} is ${weather.forecast.forecastday[1].day.condition.text}`
+  );
+  console.log(
+    `the weather for ${weather.forecast.forecastday[2].date} is ${weather.forecast.forecastday[2].day.condition.text}`
+  );
+}
+
+getWeather(city);
+
