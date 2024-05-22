e751698c3056e1d18bb090e4c1f8e6279289c85e
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-23 00:51:13 +0300

    Add a default country to display && add some style

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const userInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
 let city;
 
+displayWeather("Amman");
+
 searchButton.addEventListener("click", (event) => {
   city = userInput.value;
   displayWeather(city);
